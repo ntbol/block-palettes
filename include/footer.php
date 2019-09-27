9c4d2b2cfbd0df3551f3d469905e097e7bb4888e
unknown <ntboland@gmail.com>
2019-09-27 19:34:11 -0400

    gloabal footer and nav, global font styles and bootstrap 4

---
+++ include/footer.php
@@ -0,0 +1,25 @@
+<div class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<h5>About</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec arcu posuere, suscipit lorem eu, dictum sapien. Nullam at felis eu elit dictum ornare. Ut ex est, pellentesque a rutrum vitae, viverra eget lectus.</p>
+			</div>
+			<div class="col-md-3">
+				<h5>Share</h5>
+				<a href="#" class="theme-link">Send via e-mail</a><br>
+				<a href="#" class="theme-link">Share on Twitter</a><br>
+				<a href="#" class="theme-link">Share on Facebook</a><br>
+				<a href="#" class="theme-link">Share on Google Plus</a><br>
+				<a href="#" class="theme-link">Share on LinkedIn</a><br>
+			</div>
+			<div class="col-md-3">
+				<h5>More</h5>
+				<a href="#" class="theme-link">Contact Me</a><br>
+				<a href="#" class="theme-link">Subscribe</a><br>
+				<a href="#" class="theme-link">Submit</a><br>
+				<a href="#" class="theme-link">GitHub</a><br>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ include/header.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark navbar-theme">
+    <div class="container">
+      <a class="navbar-brand" href="#"><h3>Block Palettes</h3></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto ">
+
+        </ul>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item" style="padding-right: 25px">
+            <a class="nav-link" href="#"><h6>Palettes</h6></a>
+          </li>
+          <li class="nav-item" style="padding-right: 25px">
+            <a class="nav-link" href="#"><h6>Submit</h6></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><h6>Follow</h6></a>
+          </li>
+      </ul>
+      </div>
+    </div>
+</nav>
